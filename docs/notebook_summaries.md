e02f044cc7f72227da67399e09954f24f1be19b7
HippolyteLC <h.lecarreres1@gmail.com>
2026-04-15 16:57:08 +0200

    refactored logistic map gs, wrote nb summaries for nb 2,3, cleaned nb 2

--- docs/notebook_summaries.md
+++ docs/notebook_summaries.md
@@ -1,7 +1,12 @@
-### src\grains\1_grain_synthesis.ipynb
+### src\grains\3_stochastic_gs.ipynb
 - Extracted slice indexes from "corpus\metro_sample_2\input.wav" 
 - Computing chroma_cqts (Constant-Q Transform) for each grain, to extract the chromatic note of each grain
 - Implemented asynchronous granular synthesis with randomized parametre selection
 - Implemented granular synthesis with logistic map for grain start position selection: this algorithms also includes some stochastic grain parameterization
+
+### src\grains\2_grain_analysis.ipynb
+- Clustered predetermined grains based on all descriptors in grains.json
+- Linear interpolation from a given starting point to and end point in the descriptor space is used to compute the n nearest neighbours along this trajectory. These are concatenated into an output audio array
+
 
 
--- src/grains/algorithms.py
+++ src/grains/algorithms.py
@@ -55,3 +55,89 @@ def async_gs(input_path, output_file_dir, data, grain_indices,
     if np.max(np.abs(output_buffer)) > 0:
         output_buffer = output_buffer / np.max(np.abs(output_buffer))
     sf.write(output_asynchronous, output_buffer, samplerate=sr)
+
+
+
+def logistic_map_gs(input_path, output_file_dir, data, grain_indices, 
+             sr, n_iterations, r_start, r_end, x_start, 
+             cloud_duration, max_grain_size, steepness, 
+             rand_seed=None) -> None:
+    # logistic mapping function
+# x_n1 = r*x_n*(1-x_n)
+    """
+    If we have a time subsequence t in which we can organize some grain(s) according to the variable x_n+1 
+    decided by its prior value x_n, where the range of increasing r (typically, 0-4) relates to our total
+    time sequences T. 
+    Can use x_n-1 to determine ratio of value in the left and right channel? 
+
+    This function uses a logistic map where the parametre r is mapped to our iterations. In each iteration
+    a buffer is created in which a single grain is positioned based on the x_n value. Other grain controls, such
+    as panning, are currently determined stochastically. 
+    
+    input_path: string representing input.wav file location
+    output_file_dir: string representing outout *.wav file location
+    data: array containing the slice indices of grains in original array
+    grain_indices: array containing the indices of selected grain in the data array
+    sr: samplerate
+    cloud_duration: duration of an asynchronous cloud
+    n_streams: number of streams, where each stream has one unique grain from which it samples
+    max_grain_density: maximum grain density per stream in Hz (per second)
+    rand_seed: np random seed
+    
+    """
+    audio_arr, sr = sf.read(input_path, samplerate=sr)
+    incr = (r_end-r_start)/n_iterations
+    x_n = x_start
+    t_seconds = cloud_duration
+    t_samples = int(t_seconds * sr)
+    output_buffer = np.array([[],[]])
+    output_lmgs = output_file_dir + f"output/logistic_map_gs_{n_iterations}_iter_{r_start}_r_start_{r_end}_r_end_{x_n}_x_start_{t_seconds}_t_seconds.wav"
+
+    for _ in range(n_iterations):
+
+        grain_buffer = np.zeros(t_samples)
+        grain_idx = np.random.choice(grain_indices) # grain idx within grains_chroma array
+        grain_arr_idx = data[grain_idx] # grain index within main audio array
+        grain_size = np.random.randint(10, np.min([max_grain_size,t_samples]))
+        grain = audio_arr[grain_arr_idx: grain_arr_idx+grain_size]
+        grain_buffer_size = len(grain_buffer)
+        grain_pos = int(x_n * t_samples // 1)
+        grain_end = grain_pos+grain_size if not grain_size+grain_pos > grain_buffer_size else grain_buffer_size
+        grain_len = len(grain) if grain_end-grain_pos > len(grain) else grain_end-grain_pos
+        grain = grain[:grain_len]
+        # exp window 
+        lin_func = np.linspace(0, steepness, grain_len) # 5 here is arbitrary, controls the steepness
+        window = 1 - np.exp(-lin_func)
+        window = (window - window.min()) / (window.max() - window.min()) # normalize
+        # window = np.hanning(grain_len)
+        grain_buffer[grain_pos:grain_end] += grain*window
+
+        channel = 1 if np.random.uniform() < 0.5 else 0 # uniform selection of channel assignment of amplitudes
+        # print(channel)
+        x_n = r_start*x_n*(1-x_n)
+        x_n_1 = r_start*x_n*(1-x_n)
+        r_start += incr
+        l, r = x_n, x_n_1
+        empty_grain_buffer = np.zeros(grain_buffer_size)
+
+        # if channel == 1:
+        #     grain_buffer = np.array([grain_buffer, empty_grain_buffer]) # maybe use vstack here instead. 
+        # else:
+        #     grain_buffer = np.array([empty_grain_buffer, grain_buffer])
+
+        if channel == 1:
+            # print(grain_buffer.shape)
+            # print(l,r)
+            new_buffer = np.array([l*grain_buffer, r*grain_buffer])
+        else:
+            new_buffer = np.array([r*grain_buffer, l*grain_buffer])
+        # print(grain_buffer.shape)
+        # print(output_buffer.shape)
+        output_buffer = np.concatenate((output_buffer, new_buffer), axis=1)
+
+    if np.max(np.abs(output_buffer)) > 0:
+        output_buffer = output_buffer / np.max(np.abs(output_buffer))
+    output_buffer = output_buffer.T #np.reshape(output_buffer, (output_buffer.shape[1], output_buffer.shape[0]))
+    sf.write(output_lmgs, output_buffer, samplerate=sr)
+
+    
